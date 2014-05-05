ace96a2b0c7789209c5a89a4525524b114bb261f
gvgrao <venugopala.gorantla@gmail.com>
2014-05-05 17:16:47 -0400

    Inital version of App

---
+++ src/main/java/com/ofcc/app/App.java
@@ -0,0 +1,13 @@
+package com.ofcc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
