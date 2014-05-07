ab0201de0435daf4c0971184478d5044c1b4c642
gvgrao <venugopala.gorantla@gmail.com>
2014-05-06 22:28:57 -0400

    Usecase3 Constants file

---
+++ src/test/java/com/ofcc/app/OfccConstants.java
@@ -0,0 +1,10 @@
+package com.ofcc.app;
+
+import java.math.BigDecimal;
+
+public class OfccConstants {
+	
+	public static final BigDecimal percentage = new BigDecimal(20);
+	public static final BigDecimal incriment = new BigDecimal(100000);
+	public static final BigDecimal ranges = new BigDecimal(3);
+}
