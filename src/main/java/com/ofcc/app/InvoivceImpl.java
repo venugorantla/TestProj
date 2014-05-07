32462c0e041abebfd21598f1e8a4d10aeb53b794
gvgrao <venugopala.gorantla@gmail.com>
2014-05-06 23:10:54 -0400

    Modified for Usecase 3

--- src/main/java/com/ofcc/app/InvoivceImpl.java
+++ src/main/java/com/ofcc/app/InvoivceImpl.java
@@ -1,21 +1,41 @@
 package com.ofcc.app;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class InvoivceImpl implements InvoiceI {
 
-	private static final BigDecimal CONSTANT_100THOUSAND = new BigDecimal("100000");
-	private static final BigDecimal CONSTANT_500THOUSAND = new BigDecimal("500000");
-	
-
-	public Fee caliculateFee(Invoice invoice) {
+		public Fee caliculateFee(Invoice invoice) {
 		
 		Fee fee = new Fee();
 		// TODO Auto-generated method stub
 		if(invoice!=null && invoice.getInvoiceAmt().signum()==1)
 		{
 			BigDecimal invoiceAmt=invoice.getInvoiceAmt();
-			if( invoiceAmt.longValue() <= CONSTANT_100THOUSAND.longValue() )
+			BigDecimal bgAmt = BigDecimal.ZERO;
+			BigDecimal restAmt = BigDecimal.ZERO;
+			int j=0;
+			for(int i=OfccConstants.ranges.intValue() ; i>0; i--)
+			{
+				if(invoiceAmt.longValue()>(i-1)*OfccConstants.incriment.longValue())
+				{
+					bgAmt = (invoiceAmt.subtract(new BigDecimal((i-1)*OfccConstants.incriment.longValue()))).multiply((OfccConstants.percentage.divide(new BigDecimal(i),2, RoundingMode.HALF_EVEN)));
+					j=i-1;
+					break;
+				
+				}
+			}
+			
+			for(int i=j ; i>0;i--)
+			{
+				restAmt = restAmt.add((OfccConstants.incriment.multiply(((OfccConstants.percentage.divide(new BigDecimal(i),2, RoundingMode.HALF_EVEN))))));
+			}
+			
+			fee.setFeeAmt(bgAmt.add(restAmt));
+			System.out.println("bg fee"+bgAmt.longValue());
+			System.out.println("rest fee"+restAmt.longValue());
+			System.out.println("Total fee"+fee.getFeeAmt().longValue());
+			/*if( invoiceAmt.longValue() <= CONSTANT_100THOUSAND.longValue() )
 			{
 				
 				fee.setFeeAmt(invoiceAmt.multiply(new BigDecimal("0.2")));
@@ -35,7 +55,7 @@ public class InvoivceImpl implements InvoiceI {
 						new BigDecimal("100000").multiply(new BigDecimal("0.2")).add((new BigDecimal("400000").multiply(new BigDecimal("0.1")))).add(bgAmt.multiply(new BigDecimal("0.05")))
 						);
 				
-			}
+			}*/
 		}
 		
 		return fee;
